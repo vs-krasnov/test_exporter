591c4e4de27e894acb25b477f15633c4f2c850e8
Vsevolod Krasnov <vsevolod.krasnov@gmail.com>
2021-04-07 11:05:18 +0300

    Initial commit

---
+++ src/functions.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @param {string} text 
+ * @param {string} indentationString 
+ * 
+ * @returns {string}
+ */
+ function createDocumentationComment(text, indentationString) {
+    return "<!--"
+}
+
+
+Pulsar.registerFunction("createDocumentationComment", createDocumentationComment)
